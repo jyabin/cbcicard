4da12fae781e6e2f5a27f0b68335557c8e1bc1f8
admin <jy.abin@gmail.com>
2019-05-16 11:03:25 -0400

    modified file `wordfence-waf.php`

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/homepages/9/d657751990/htdocs/clickandbuilds/CBCICARD/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/homepages/9/d657751990/htdocs/clickandbuilds/CBCICARD/wp-content/wflogs/');
+	include_once '/homepages/9/d657751990/htdocs/clickandbuilds/CBCICARD/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
